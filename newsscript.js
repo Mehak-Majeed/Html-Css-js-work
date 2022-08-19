d3acaf5cedddb457f2b3b1ed1bf11c73d7eab710
Mehak-Majeed <mehakmajeed786786@gmail.com>
2022-08-19 18:17:22 +0400

    Added separate  kash page

--- newsscript.js
+++ newsscript.js
@@ -1,9 +1,29 @@
 
-function autoFun(){
-	alert('you submitted ')
-	var emailValue = document.getElementById("emailid").value;
-	var auto = document.getElementById("nav-menu");
-	alert(auto)
+
+function Fun() {
+ 
+  var tm= document.getElementById('navigation');
+
+  var lists= document.getElementById('items');
+  console.log(lists.style.display);
+  if(lists.style.display=='' || lists.style.display=='block'){
+	 
+	  lists.style.display='none'
+	  tm.style.minWidth='20px'
+	 
+	   tm.style.borderLeft='none';
+		       tm.style.background='none';
+  }
+  else if(lists.style.display!=='block'){
+	   lists.style.display='block'
+	   tm.style.minWidth='300px'
+	      tm.style.borderLeft=' 5px solid #9c27b0';
+		       tm.style.background=' #d19bd1';
+	   
+  }
+  
+ 
+  
 }
 /*
 nav ul{
